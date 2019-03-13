57e1e75407137173d9f27471e1dcb0851fb8d306
lindwurm <lindwurm.q@gmail.com>
2019-03-13 18:45:24 +0900

    clover: LineageOS!
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- lineage_clover.mk
+++ lineage_clover.mk
@@ -23,11 +23,11 @@ $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/clover.mk)
 # Inherit from clover device
 $(call inherit-product, device/xiaomi/clover/device.mk)
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_tablet_wifionly.mk)
-$(call inherit-product, vendor/mk/config/telephony.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/telephony.mk)
 
-PRODUCT_NAME := mk_clover
+PRODUCT_NAME := lineage_clover
 PRODUCT_BRAND := Xiaomi
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_DEVICE := clover
